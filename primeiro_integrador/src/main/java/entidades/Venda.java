47e70ccda29704f71c165b53d96061ee342600db
DojanDev <272136@unifio.edu.br>
2025-09-03 22:59:17 +0000

    TrabalhoParcial

---
+++ primeiro_integrador/src/main/java/entidades/Venda.java
@@ -0,0 +1,26 @@
+package entidades;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+    public LocalDate data;
+    public Double valorTotal;
+    public Integer quantidadeTotal;
+
+    @ManyToOne
+    public Cliente cliente;
+
+    @ManyToOne
+    public Funcionario funcionario;
+
+}
