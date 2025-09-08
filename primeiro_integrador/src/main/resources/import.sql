586862068a943dc7ff9f3abfc9cf27a64651c710
DojanDev <272136@unifio.edu.br>
2025-09-08 14:25:25 +0000

    Segundo TrabalhoParcial

--- primeiro_integrador/src/main/resources/import.sql
+++ primeiro_integrador/src/main/resources/import.sql
@@ -1,23 +1,89 @@
+
 insert into Categoria (nome) values ('Bebidas');
 insert into Categoria (nome) values ('Carne');
+insert into Categoria (nome) values ('Laticínios');
+insert into Categoria (nome) values ('Padaria');
+insert into Categoria (nome) values ('Higiene');
+insert into Categoria (nome) values ('Limpeza');
+insert into Categoria (nome) values ('Cereais');
+insert into Categoria (nome) values ('Congelados');
+insert into Categoria (nome) values ('Bebidas Alcoólicas');
+insert into Categoria (nome) values ('Frutas');
+insert into Categoria (nome) values ('Verduras');
+insert into Categoria (nome) values ('Doces');
+
 insert into Fabricante (nome) values ('JBS');
 insert into Fabricante (nome) values ('The Coca Cola');
-insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Coca Cola', 100, 7.50, '2025-09-01', 1, 2);
-insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Carne', 50, 25.00, '2024-06-15', 2, 1);
-insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Frango', 80, 15.00, '2024-07-20', 3, 1);
-insert into Produto(nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Suco de Laranja', 120, 5.00, '2025-08-10', 4, 2);
-insert into Funcionario (nome, cargo, salario) values ('Edvan', 'Gerente', 4500.00);
-insert into Funcionario (nome, cargo, salario) values ('Ramon', 'Vendedor', 3000.00);
-insert into Funcionario (nome, cargo, salario) values ('Marcelo', 'Caixa', 2500.00);
-insert into Funcionario (nome, cargo, salario) values ('Gustavo', 'Estoquista', 2800.00);
-insert into Cliente (nome, email, telefone) values ('Edvan', 'edvan1552@gmail.com', '1234-5678');
-insert into Cliente (nome, email, telefone) values ('Gustavo', 'gustavo2442@gmail.com', '9876-5432');
-insert into Cliente (nome, email, telefone) values ('Ramon', 'ramon6632@gmail.com', '4321-8765');
-insert into Cliente (nome, email, telefone) values ('Marcelo', 'marcelo1234@gmail.com', '8765-4321');
-insert into Venda (data, total, funcionario_codigo, cliente_codigo) values ('2024-06-01', 75.00, 2, 1);
-insert into Venda (data, total, funcionario_codigo, cliente_codigo) values ('2024-06-02', 50.00, 2, 2);
-insert into ItemVenda (venda_codigo, produto_codigo, quantidade, preco) values (1, 1, 5, 7.50);
-insert into ItemVenda (venda_codigo, produto_codigo, quantidade, preco) values (1, 2, 1, 25.00);
-insert into ItemVenda (venda_codigo, produto_codigo, quantidade, preco) values (2, 3, 2, 15.00);
-insert into ItemVenda (venda_codigo, produto_codigo, quantidade, preco) values (2, 4, 5, 5.00);     
-insert into ItemVenda (venda_codigo, produto_codigo, quantidade, preco) values (1, 4, 3, 5.00);
+insert into Fabricante (nome) values ('Nestlé');
+insert into Fabricante (nome) values ('Unilever');
+insert into Fabricante (nome) values ('Ambev');
+insert into Fabricante (nome) values ('PepsiCo');
+insert into Fabricante (nome) values ('BRF');
+insert into Fabricante (nome) values ('Perdigão');
+insert into Fabricante (nome) values ('Danone');
+insert into Fabricante (nome) values ('Sadia');
+insert into Fabricante (nome) values ('Heineken');
+insert into Fabricante (nome) values ('Aurora');
+
+insert into Produto (nome, quantidade, preco, validade) values ('Coca Cola', 100, 7.50, '2025-09-01');
+insert into Produto (nome, quantidade, preco, validade) values ('Carne', 50, 25.00, '2024-06-15');
+insert into Produto (nome, quantidade, preco, validade) values ('Leite Integral', 200, 4.20, '2024-12-10');
+insert into Produto (nome, quantidade, preco, validade) values ('Pão Francês', 500, 0.50, '2024-06-05');
+insert into Produto (nome, quantidade, preco, validade) values ('Sabonete Dove', 300, 3.80, '2026-01-01');
+insert into Produto (nome, quantidade, preco, validade) values ('Detergente Ypê', 400, 2.10, '2027-03-15');
+insert into Produto (nome, quantidade, preco, validade) values ('Arroz 5kg', 100, 22.00, '2025-11-20');
+insert into Produto (nome, quantidade, preco, validade) values ('Pizza Congelada', 50, 18.50, '2025-03-15');
+insert into Produto (nome, quantidade, preco, validade) values ('Cerveja Heineken', 350, 5.50, '2026-07-01');
+insert into Produto (nome, quantidade, preco, validade) values ('Salgadinho', 120, 6.00, '2024-06-06');
+insert into Produto (nome, quantidade, preco, validade) values ('Alface Crespa', 80, 3.00, '2024-06-04');
+insert into Produto (nome, quantidade, preco, validade) values ('Chocolate Nestlé', 200, 7.90, '2025-02-10');
+
+insert into Funcionario (nome) values ('Edvan');
+insert into Funcionario (nome) values ('Ramon');
+insert into Funcionario (nome) values ('Marcelo');
+insert into Funcionario (nome) values ('Gustavo');
+insert into Funcionario (nome) values ('Lucas');
+insert into Funcionario (nome) values ('Bruno');
+insert into Funcionario (nome) values ('Fernanda');
+insert into Funcionario (nome) values ('Carla');
+insert into Funcionario (nome) values ('Patrícia');
+insert into Funcionario (nome) values ('Roberto');
+
+
+insert into Cliente (nome) values ('Edvan');
+insert into Cliente (nome) values ('Gustavo');
+insert into Cliente (nome) values ('Ramon');
+insert into Cliente (nome) values ('Marcelo');
+insert into Cliente (nome) values ('Ana Clara');
+insert into Cliente (nome) values ('João Pedro');
+insert into Cliente (nome) values ('Maria Eduarda');
+insert into Cliente (nome) values ('Pedro Henrique');
+insert into Cliente (nome) values ('Larissa');
+insert into Cliente (nome) values ('Fernanda');
+
+
+insert into Venda (data) values ('2024-06-01');
+insert into Venda (data) values ('2024-06-02');
+insert into Venda (data) values ('2024-06-03');
+insert into Venda (data) values ('2024-06-04');
+insert into Venda (data) values ('2024-06-05');
+insert into Venda (data) values ('2024-06-06');
+insert into Venda (data) values ('2024-06-07');
+insert into Venda (data) values ('2024-06-08');
+insert into Venda (data) values ('2024-06-09');
+insert into Venda (data) values ('2024-06-10');
+
+
+insert into ItemVenda (valorParcial, quantidadeParcial) values (2.5, 1500);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (15.00, 10);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (30.00, 20);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (50.00, 25);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (7.50, 3);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (100.00, 40);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (12.00, 6);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (80.00, 15);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (25.00, 5);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (60.00, 12);
+insert into ItemVenda (valorParcial, quantidadeParcial) values (45.00, 9);
+
+
